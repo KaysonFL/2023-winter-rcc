f67a98a6269dec904c300b6083a56f112e5195a3
Kayson Lopez <klopez221@student.rccd.edu>
2023-01-05 13:10:04 -0800

    Signed-off-by: Kayson Lopez <klopez221@student.rccd.edu>

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Kayson Lopez
+ * Created on January 5, 2023, 12:47 PM
+ * Purpose: Hello World
+ */
+
+//System Libraries
+#include <iostream> //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    cout<<"Hello World"<<endl;
+    //Clean up memory and files
+    
+    //Exit the Program
+    return 0;
+}
+
---
+++ Hmwk/cpp template/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Kayson Lopez
+ * Created on January 5, 2023, 12:47 PM
+ * Purpose: Template to be used for all
+ *          future Hmwk, Labs, Exams, Projects
+ */
+
+//System Libraries
+#include <iostream> //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    
+    //Clean up memory and files
+    
+    //Exit the Program
+    return 0;
+}
+
